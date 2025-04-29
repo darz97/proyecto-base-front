5e9ceebe6d1bd35e36bb4e89531b4bec2bbc7f55
Diego Ramírez <diegoaramirez2009@gmail.com>
2025-04-28 23:50:19 -0500

    TASk001:se crea paqueteria inicial

--- src/app/core/service/prueba.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-import { environment } from '@env/environment.development';
-
-@Injectable({
-  providedIn: 'root',
-})
-export class PruebaService {
-  environment: string = environment.apiUrl;
-}
